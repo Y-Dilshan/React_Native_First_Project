84002aede53c85218febda7a6e1941856af419d2
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-06-22 12:00:39 +0530

    created login function

--- controller/authController.js
+++ controller/authController.js
@@ -60,3 +60,53 @@ const signup = async (req, res) => {
         })
     }
 }
+
+// @desc Login user
+// @desc POST /api/v1/auth/login
+// @access public
+const login = async(req, res) => {
+    try{
+        const {email, password} = req.body;
+
+        // Validate
+        if(!email || !password){
+            return res.status(400).json({
+                success : false, 
+                message : "Please provide email or password"
+            })
+        }
+
+        const user = await User.findOne({email}).select('+password');
+
+        if(user){
+            return res.status(401).json({
+                success : false,
+                message : "Invalid creadentials"
+            })
+        }
+
+        // Check is password matches
+        const isMatch = await user.matchPassword(password);
+        if(!isMatch){
+            return res.status(401). json({
+                success : false,
+                message : 'Inavlid credentials'
+            })
+        }
+
+        res.status(200).json({
+                success : true,
+                data : {
+                    _id : user._id,
+                    name : user.name,
+                    email : user.email,
+                    token:generateToken(user._id)
+                }
+            })
+    }catch(error){
+        res.status(500).json({
+            success : false,
+            message : error.message
+        })
+    }
+}
